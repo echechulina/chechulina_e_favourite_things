438e4101ed305a64797e296679d7de87bca20fca
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-11-15 21:37:51 -0500

    bugs fixing

---
+++ database/tbl_things.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 15, 2020 at 10:01 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_favourite_things`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_things`
+--
+
+CREATE TABLE `tbl_things` (
+  `ID` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `image` varchar(20) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_things`
+--
+
+INSERT INTO `tbl_things` (`ID`, `title`, `image`, `description`) VALUES
+(1, 'Painting', 'painting.jpg', 'Drawing is one of the most challenging, amazing and interesting activities for me. Painting is a long process that requires attention, patience, mood and skill development. Each of my paintings is a challenge to myself, this is an immersion in the topic and a thorough study of details. I am happy and filled with the energy of creativity when I paint.'),
+(2, 'Postcards', 'postcard.jpg', 'For many years I have been interested in collecting postcards. The subject of my attention is unusual drawings and fonts. I am happy to bring atypical travel postcards from my travels, which have accumulated over 200!'),
+(3, 'Board games', 'game.jpeg', 'Board games are a separate chapter of my life, which is filled with vivid emotions of joy from victory and disappointment from defeat. Logic, associative, quests, arcades - I love them all! And beautiful, unusual graphics make my love even stronger!');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_things`
+--
+ALTER TABLE `tbl_things`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_things`
+--
+ALTER TABLE `tbl_things`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- js/main.js
+++ js/main.js
@@ -1,55 +1,76 @@
-   import { fetchData } from "./modules/TheDataMiner.js";
+import { fetchData } from "./modules/TheDataMiner.js";
 
-   (() => {
-       console.log('loaded');
-       
-       function popErrorBox(message) {
-           alert("Something broke... I have to refuse the connection.");
-       }
+
+(() => {
    
-       function handleDataSet(data) {
+    console.log('loaded');
+
+    function popErrorBox(message) {
+        alert("Something broke. You can no longer view the page.");
+    }
+
+    function lightboxSetUp(event) {
+       
+        const lightbox = document.querySelector(".lightbox");
+        const data = event.currentTarget.dataset;
+        console.log(data);
+        lightbox.querySelector(".lightImage").src = data.image;
+        lightbox.querySelector(".thingTitle").innerHTML = data.title;
+        lightbox.querySelector(".thingDescription").innerHTML = data.description;
+        lightbox.classList.add('openLB');
+
+    }
+
+    function hideLB(event) {
+        const lightbox = document.querySelector(".lightbox");
+        lightbox.classList.remove('openLB');
+    }
+
+
+
+
+    function retrieveProjectInfo(event) {
+       
+        if (!event.target.id) { return }
+
+        fetchData(`./includes/index.php?ID=${event.target.id}`).then(data => console.log(data)).catch(err => {console.log(err); popErrorBox(err);});
+    }
+
+
+
+    function renderPortfolioThumbnails(thumbs) {
+        console.log(thumbs)
         let thingSection = document.querySelector('.thing-section'),
             thingTemplate = document.querySelector('#thing-template').content;
 
-        for (let thing in data) {
+        for (let thing in thumbs) {
+
+
             let currentThing = thingTemplate.cloneNode(true),
-                currentThingText = currentThing.querySelector('.thing').children;
+                currentThingText = currentThing.querySelector('.thing').children,
+                currentThingLB = currentThing.querySelector('.thing');
 
-            currentThingText[1].src = `images/${data[thing].Image}`;
-            currentThingText[2].textContent = data[thing].Title;
-            currentThingText[3].textContent = data[thing].Description;
+
+            currentThingText[1].src = `images/${thumbs[thing].image}`;
+            currentThingText[1].id = thumbs[thing].ID;
             
 
-            // add this new user to the view
-            userSection.appendChild(currentUser);
+            currentThingLB.dataset.title = thumbs[thing].title;
+            currentThingLB.dataset.image = `images/${thumbs[thing].image}`;
+            currentThingLB.dataset.description = thumbs[thing].description;
+
+            currentThingLB.addEventListener("click", lightboxSetUp);
+
+
+            thingSection.appendChild(currentThing);
         }
-    }
-   
-    function retrieveProjectInfo() {
-        // test for an ID
-        debugger;
-        console.log(this.id);
 
-        fetchData(`./includes/index.php?id=${this.id}`).then(data => console.log(data)).catch(err => console.log(err));
+        thingSection.addEventListener("click", retrieveProjectInfo);
     }
-   
-       function renderPortfolioThumbnails(thumbs) {
-           let thingSection = document.querySelector('.thing-section'),
-               thingTemplate = document.querySelector('#thing-template').content;
-   
-           for (let thing in thumbs) {
-               let currentThing = thingTemplate.cloneNode(true),
-                   currentThingText = currentThing.querySelector('.thing').children;
-   
-               currentThingText[1].src = `images/${thumbs[thing].Image}`;
-               currentThingText[1].id = thumbs[thing].id;
-            
-               // add this new user to the view
-              
-               thingSection.appendChild(currentThing);
-           }
-          thingSection.addEventListener("click", retrieveProjectInfo);
-       }
-           
-       fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => console.log(err));
-   })();
+
+    fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => {console.log(err); popErrorBox(err);});
+
+    let lbClose = document.querySelector('.close-lb');
+    lbClose.addEventListener('click', hideLB);
+    lbClose.addEventListener('click', hideLB);
+})();
