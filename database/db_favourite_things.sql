cd482661f1ace2d01496ab63945cac6b70a745db
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2020-11-15 23:29:51 -0500

    Uploading database

--- database/db_favourite_things.sql
+++ database/db_favourite_things.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 15, 2020 at 10:01 PM
+-- Generation Time: Nov 16, 2020 at 04:27 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
